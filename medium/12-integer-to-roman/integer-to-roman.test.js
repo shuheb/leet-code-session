8080b94b8c9bdede19da01a1cb38529f916a43c6
shuheb <37620997+shuheb@users.noreply.github.com>
2022-03-08 20:43:23 +0000

    added more

---
+++ medium/12-integer-to-roman/integer-to-roman.test.js
@@ -0,0 +1,20 @@
+
+test('Integer to Roman 1', () => {
+    expect(intToRoman(3)).toEqual('III');
+})
+
+test('Integer to Roman 2', () => {
+    expect(intToRoman(5)).toEqual('V');
+})
+
+test('Integer to Roman 3', () => {
+    expect(intToRoman(3424)).toEqual('MMMCDXXIV');
+})
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+
+};
