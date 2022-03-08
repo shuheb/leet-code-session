95f30e0032ee56ed51a61a0f22ffe444a50fd044
shuheb <37620997+shuheb@users.noreply.github.com>
2022-03-08 13:44:06 +0000

    init

---
+++ easy/remove-element/remove-element.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/remove-element/
+
+test.skip('Remove Element Test Case 1', () => {
+    expect(removeElement([3, 2, 2, 3], 3)).toBe(2)
+});
+
+test.skip('Remove Element Test Case 2', () => {
+    expect(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)).toBe(5)
+})
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+   
+};
